f9a6554fc6256ed1e9a0c4cc8a382e392c540b49
Glen Bradley <glen.haber@thebridgecorp.com>
2023-11-16 00:15:42 -0500

    Make buttons responsive

--- wyr/would_you_rather.py
+++ wyr/would_you_rather.py
@@ -8,15 +8,26 @@ class WouldYouRather(tk.Frame):
 
         tk.Label(text='Would you rather...').pack()
 
+        self.value_a = tk.StringVar(value='...fight 100 duck-sized horses?')
+        self.value_b = tk.StringVar(value='...fight 1 horse-sized duck?')
+
         self.remarks = tk.StringVar()
         self.response = tk.Label(textvariable=self.remarks)
         self.response.pack(side='bottom')
 
-        self.a = tk.Button(text='...fight 100 duck-sized horses?')
-        self.a.pack(side='left')
+        self.choice_a = tk.Button(textvariable=self.value_a)
+        self.choice_a.pack(side='left')
+        self.choice_a.bind('<Button-1>', self.snide_remarks)
         tk.Label(text='or').pack(side='left')
-        self.b = tk.Button(text='...fight 1 horse-sized duck?')
-        self.b.pack(side='left')
+        self.choice_b = tk.Button(textvariable=self.value_b)
+        self.choice_b.pack(side='left')
+        self.choice_b.bind('<Button-1>', self.snide_remarks)
+
+    def snide_remarks(self, event):
+        if event.widget is self.choice_a:
+            self.remarks.set('Protect your shins!')
+        else:
+            self.remarks.set('Distract it with apples!')
 
 
 root = tk.Tk()
